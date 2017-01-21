b35ea674913ee5d5f41f2fec62e8c28a5f52722a
dominikspeh <mail@domi-speh.de>
2017-01-21 23:38:47 +0100

    Added second progress bar

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,21 @@
 moment.locale('de');
 
 var clock = Vue.component('current-time', {
-    template: '<div>' +
+    template: '<div class="text-center">' +
     '<h1 class="big">{{time}} Uhr</h1>' +
+    '<div class="progress">'+
+    '<div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" v-bind:style="{width : progress }">'+
+    '</div></div>' +
     '<p class="lead">{{date}}</p>' +
-    '<span></span>' +
     '</div>',
     created: function () {
 
         var clock = this;
         setInterval(function () {
             clock.time = moment().format("HH:mm");
-            clock.day = moment().format("dddd, DD. MMMM YYYY")
+            clock.day = moment().format("dddd, DD. MMMM YYYY");
+            clock.progress = moment().format("ss")*1.7+"%";
+
 
         },1000)
 
@@ -20,8 +24,10 @@ var clock = Vue.component('current-time', {
     data: function () {
         return {
             time: moment().format("HH:mm"),
-            date: moment().format("dddd, DD. MMMM YYYY")
-        }
+            date: moment().format("dddd, DD. MMMM YYYY"),
+            progress: moment().format("ss")*1.7+"%",
+
+    }
     }
 })
 
